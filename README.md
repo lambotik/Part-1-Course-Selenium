f24250f7ad73942dc18878387323f0f0d0b194c9
Дима <lambotik1988@gmail.com>
2022-10-23 20:19:43 +0300

    - Урок 3.4 ООП наследование класса

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - Урок 2.24 Примитивный калькулятор [calculator.py](https://github.com/lambotik/Automation-and-Programming-by-Python-and-Selenium/blob/main/calculator.py)
 - Урок 3.1 ООП создание классов и атрибутов класса [person.py](https://github.com/lambotik/Automation-and-Programming-by-Python-and-Selenium/blob/main/person.py)
 - Урок 3.2 ООП создание классов и атрибутов класса [people.py](https://github.com/lambotik/Automation-and-Programming-by-Python-and-Selenium/blob/main/people.py)
+- Урок 3.4 ООП создание классов и атрибутов класса [warrior.py]()
---
+++ warrior.py
@@ -0,0 +1,55 @@
+class People():
+    """Create people"""
+
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
+        """default weight meaning for all persons"""
+        self.weight = 100
+
+    def description_person(self):
+        """Get description person"""
+        description = self.name + ' ' + str(self.age) + ' years old' + ', his height is ' + str(
+            self.height) + 'cm' + ', his weight is ' + str(self.weight) + 'kg'
+        print('New person name : ' + description)
+
+    def get_weight_person(self):
+        """Get weight person"""
+        print('Weight person is : ' + str(self.weight))
+
+    def update_weight(self, kg):
+        """Change weight person"""
+        self.weight = kg
+
+
+class Warrior(People):
+    """Create class warrior"""
+
+    def __init__(self, name, age, height):
+        """Get attribute of parent class"""
+        super().__init__(name, age, height)
+        self.rage = 100
+
+    def get_rage(self):
+        print('Conan rage equal ' + str(self.rage))
+
+    def description_person(self):
+        """Переопределение метода родителя"""
+        description = self.name + ' ' + str(self.age) + ' years old ' + ' and his charge of rage equal ' + str(self.rage)
+        print('New warrior name is ' + description)
+
+
+# dima = People('Dima', 34, 172)
+# dima.description_person()
+# """get default weight person"""
+# dima.get_weight_person()
+# """change weight person"""
+# dima.update_weight(65)
+# """get update weight person"""
+# dima.get_weight_person()
+
+conan = Warrior('Conan', 30, 120)
+conan.get_weight_person()
+conan.get_rage()
+conan.description_person()
