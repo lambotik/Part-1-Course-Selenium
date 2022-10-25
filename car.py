5c4868c5d14cca6478c75f9cc783d06f179e08f5
Дима <lambotik1988@gmail.com>
2022-10-25 22:17:33 +0300

    - Урок 3.6 ООП наследование класса!

--- car.py
+++ car.py
@@ -1,4 +1,5 @@
 class Car():
+    """Create class car"""
     def __init__(self, model, year, engine_volume, price, mileage):
         self.model = model
         self.year = year
@@ -8,11 +9,13 @@ class Car():
         self.wheels = 4
 
     def car(self):
+        """Create car"""
         print('Car name: ' + self.model + '\nYear of issue:' + str(self.year) + '\nEngine volume: ' + str(
             self.engine_volume) + '\nPrice: ' + str(self.price) + '$' + '\nMileage: ' + str(
             self.mileage) + 'km' + '\nWheels: ' + str(self.wheels))
 
 class Truck(Car):
+    """Get attribute of parent class"""
     def __init__(self, model, year, engine_volume, price, mileage):
         super().__init__(model, year, engine_volume, price, mileage)
         self.wheels = 8
