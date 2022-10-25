f9e30bc7e1064071080769eccbe137214821d4c4
Дима <lambotik1988@gmail.com>
2022-10-25 22:21:45 +0300

    - Урок 3.6 ООП наследование класса!

--- car.py
+++ car.py
@@ -1,5 +1,6 @@
 class Car():
     """Create class car"""
+
     def __init__(self, model, year, engine_volume, price, mileage):
         self.model = model
         self.year = year
@@ -8,23 +9,28 @@ class Car():
         self.mileage = mileage
         self.wheels = 4
 
+    """Create car"""
+
     def car(self):
-        """Create car"""
         print('Car name: ' + self.model + '\nYear of issue:' + str(self.year) + '\nEngine volume: ' + str(
             self.engine_volume) + '\nPrice: ' + str(self.price) + '$' + '\nMileage: ' + str(
             self.mileage) + 'km' + '\nWheels: ' + str(self.wheels))
 
+
 class Truck(Car):
-    """Get attribute of parent class"""
+    """Get attribute of parent class and change num wheels"""
+
     def __init__(self, model, year, engine_volume, price, mileage):
         super().__init__(model, year, engine_volume, price, mileage)
         self.wheels = 8
+
+    """Getting a description about model"""
+
     def truck_description(self):
         description = 'Car name: ' + self.model + '\nYear of issue:' + str(self.year) + '\nEngine volume: ' + str(
             self.engine_volume) + '\nPrice: ' + str(self.price) + '$' + '\nMileage: ' + str(
             self.mileage) + 'km' + '\nWheels: ' + str(self.wheels)
         print('Truck ' + description)
-
 
 
 # volvo = Car('Volvo', 2022, 5.0, 50000, 12000)
