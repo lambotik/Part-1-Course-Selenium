a8478e4efa7a6b358334bb0cca658a0a03a394e1
lambotik <lambotik1988@gmail.com>
2022-11-12 16:36:56 +0300

    - Урок 4.13 Очистка полей [test_field_clearing.py]

---
+++ python_selenium/test_field_clearing.py
@@ -0,0 +1,26 @@
+import datetime
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+standard_user = 'standard_user'
+locked_out_user = 'locked_out_user'
+PASSWORD = 'secret_sauce'
+LOGIN = 'login-button'
+
+'''For Chrome'''
+driver = webdriver.Chrome(
+    executable_path='C:\\Users\Дима\\PycharmProjects\\Course Python and Selenium\\Automation-and-Programming-by-Python-and-Selenium\\python_selenium\\chromedriver.exe')
+
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+user_name = driver.find_element(By.ID, 'user-name')
+user_name.send_keys(standard_user)
+time.sleep(2)
+"""Очистка полей"""
+user_name.clear()
+
+time.sleep(2)
+driver.close()
